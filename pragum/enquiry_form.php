da74c6855082cc4ec271ac5f8b2757e45ae863bf
Rohit <rohitsraj12@gmail.com>
2021-09-27 11:20:23 +0530

    Update enquiry_form.php

--- pragum/enquiry_form.php
+++ pragum/enquiry_form.php
@@ -2,32 +2,31 @@
 
 if(isset($_POST['enquiry_form'])){
 
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $service = $_POST['service'];
-
+    $name       = $_POST['name'];
+    $phone      = $_POST['phone'];
+    $email      = $_POST['email'];
+    $message    = $_POST['message'];
+    $service    = $_POST['service'];
 
     //send email to
-    $to = "info@pragum.com";
-    $to1 = "dndprash296@gmail.com";
+    $to         = "info@pragum.com";
+    $to1        = "dndprash296@gmail.com";
 
-    $subject = "Enquiry form | Landing Page";
+    $subject    = "Enquiry form | Landing Page";
 
-    $message = "<p>Enquiry</p></br>";
-    $message .= "<p> name: <strong>". $name ."</strong></p></br>";
-    $message .= "<p> phone number: <strong>". $phone ."</strong></p></br>";
-    $message .= "<p> email: <strong>". $email ."</strong></p></br>";
-    $message .= "<p> Service: <strong>". $service ."</strong></p></br>";
-    $message .= "<p> Message: <strong>". $message ."</strong></p></br>";
-    $message .= "<p>Thank you,</p>";
-    $message .= "<p>". $name ."</p>";
-    $message .= "<p>" . $email . " | " . $phone . "</p>";
+    $message    = "<p>Enquiry</p></br>";
+    $message    .= "<p> name: <strong>". $name ."</strong></p></br>";
+    $message    .= "<p> phone number: <strong>". $phone ."</strong></p></br>";
+    $message    .= "<p> email: <strong>". $email ."</strong></p></br>";
+    $message    .= "<p> Service: <strong>". $service ."</strong></p></br>";
+    $message    .= "<p> Message: <strong>". $message ."</strong></p></br>";
+    $message    .= "<p>Thank you,</p>";
+    $message    .= "<p>". $name ."</p>";
+    $message    .= "<p>" . $email . " | " . $phone . "</p>";
 
-    $headers = "From:" . $name . " <" . $email . ">\r\n";
-    $headers .= "Reply-To: " . $email . "\r\n";
-    $headers .= "Content-type: text/html\r\n";
+    $headers    = "From:" . $name . " <" . $email . ">\r\n";
+    $headers    .= "Reply-To: " . $email . "\r\n";
+    $headers    .= "Content-type: text/html\r\n";
 
     mail($to, $subject, $message, $headers);
     mail($to1, $subject, $message, $headers);
