f591f1ae1bdb32726d992fa9ea9b5cb898aee7eb
Rohit <rohitsraj12@gmail.com>
2021-10-23 15:37:42 +0530

    Create script.js

---
+++ school_landing_page/assets/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $(".sticky__enquiry").fadeOut();
+
+    const mediaQuery = window.innerWidth;
+
+    if(mediaQuery < 768){
+        //small device
+        $(".hamburger").click(function(){
+            $(".header__nav").slideToggle();
+        });
+
+        $(".nav__link").click(function(){
+            $(".header__nav").slideUp();
+        });
+
+        $(window).scroll(function () {
+            var height = $(window).height();
+            var height = height;
+        
+            if ($(window).scrollTop() > height) {
+                // $(".body__header").removeClass("non__fixed-header");
+                // $(".body__header").addClass("fixed-header");
+
+                $(".sticky__enquiry").fadeIn();
+                // console.log(1000);
+            } else {
+                // $(".body__header").removeClass("fixed-header");
+                // $(".body__header").addClass("non__fixed-header");
+                $(".sticky__enquiry").fadeOut();
+                // console.log(2000);
+            }
+        });
+
+    } else {
+        // large device
+        $(window).scroll(function () {
+            var height = $(window).height();
+            var height = height / 2;
+        
+            if ($(window).scrollTop() > height) {
+                $(".body__header").removeClass("non__fixed-header");
+                $(".body__header").addClass("fixed-header");
+
+                $(".sticky__enquiry").fadeIn();
+                // console.log(1000);
+            } else {
+                $(".body__header").removeClass("fixed-header");
+                $(".body__header").addClass("non__fixed-header");
+                $(".sticky__enquiry").fadeOut();
+                // console.log(2000);
+            }
+        });
+    }
+
+})
