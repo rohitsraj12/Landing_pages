6a1892cb741080af2674879101d4c21da70acfa1
Rohit <rohitsraj12@gmail.com>
2021-10-23 17:15:52 +0530

    Update enquiry.php

--- school_landing_page/enquiry.php
+++ school_landing_page/enquiry.php
@@ -8,12 +8,27 @@
         $email = $_POST['email'];
         $message = $_POST['message'];
 
-        echo "$parent_name, $student_name, $phone, $email, $message";
+        echo "$parent_name, $student_name, $phone, $email, $message = message";
     }
 
+    if(isset($_POST['enquiry'])){
 
+        $parent_name = $_POST['parent_name'];
+        $student_name = $_POST['student_name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
 
+        echo "$parent_name, $student_name, $phone, $email = enquiry";
+    }
 
+    if(isset($_POST['brochure'])){
 
+        $parent_name = $_POST['parent_name'];
+        $student_name = $_POST['student_name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+
+        echo "$parent_name, $student_name, $phone, $email = brochure";
+    }
 
 ?>
