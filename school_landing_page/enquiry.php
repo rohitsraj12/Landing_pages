de15167bee4012da81729cccd225f9acb93fdc24
Rohit <rohitsraj12@gmail.com>
2021-11-19 12:01:30 +0530

    Update enquiry.php

--- school_landing_page/enquiry.php
+++ school_landing_page/enquiry.php
@@ -8,7 +8,27 @@
         $email = $_POST['email'];
         $message = $_POST['message'];
 
-        echo "$parent_name, $student_name, $phone, $email, $message = message";
+        // echo "$parent_name, $student_name, $phone, $email, $message = message";
+        $to         = 'officialsolarhause@gmail.com';
+        $subject    = "Subject Name";
+
+        $message    = "<h4>message</h4></br>";
+        $message    .= "<p>name: <strong>". $name ."</strong></p></br>";
+        $message    .= "<p>email: <strong>". $email ."</strong></p></br>";
+        $message    .= "<p>phone number: <strong>". $phone ."</strong></p></br>";
+        $message    .= "<p>Message: <strong>". $message ."</strong></p></br>";
+        $message    .= "<p>Thank you,</p>";
+        $message    .= "<p>". $name ."</p>";
+        $message    .= "<p>" . $email . " | " . $phone . "</p>";
+
+        $headers    = "From:" . $name . " <" . $email . ">\r\n";
+        $headers    .= "Reply-To: " . $email . "\r\n";
+        $headers    .= "Content-type: text/html\r\n";
+
+        mail($to, $subject, $message, $headers);
+
+        // Redirect to thank you page
+        header('Location:thank-you.html');
     }
 
     if(isset($_POST['enquiry'])){
@@ -18,7 +38,28 @@
         $phone = $_POST['phone'];
         $email = $_POST['email'];
 
-        echo "$parent_name, $student_name, $phone, $email = enquiry";
+        // echo "$parent_name, $student_name, $phone, $email = enquiry";
+        
+        // echo "$parent_name, $student_name, $phone, $email, $message = message";
+        $to         = 'officialsolarhause@gmail.com';
+        $subject    = "Subject Name";
+
+        $message    = "<h4>message</h4></br>";
+        $message    .= "<p>name: <strong>". $name ."</strong></p></br>";
+        $message    .= "<p>email: <strong>". $email ."</strong></p></br>";
+        $message    .= "<p>phone number: <strong>". $phone ."</strong></p></br>";
+        $message    .= "<p>Thank you,</p>";
+        $message    .= "<p>". $name ."</p>";
+        $message    .= "<p>" . $email . " | " . $phone . "</p>";
+
+        $headers    = "From:" . $name . " <" . $email . ">\r\n";
+        $headers    .= "Reply-To: " . $email . "\r\n";
+        $headers    .= "Content-type: text/html\r\n";
+
+        mail($to, $subject, $message, $headers);
+
+        // Redirect to thank you page
+        header('Location:thank-you.html');
     }
 
     if(isset($_POST['brochure'])){
@@ -28,7 +69,29 @@
         $phone = $_POST['phone'];
         $email = $_POST['email'];
 
-        echo "$parent_name, $student_name, $phone, $email = brochure";
+        // echo "$parent_name, $student_name, $phone, $email = brochure";
+        
+        // echo "$parent_name, $student_name, $phone, $email, $message = message";
+        $to         = 'officialsolarhause@gmail.com';
+        $subject    = "Subject Name";
+
+        $message    = "<h4>message</h4></br>";
+        $message    .= "<p>name: <strong>". $name ."</strong></p></br>";
+        $message    .= "<p>email: <strong>". $email ."</strong></p></br>";
+        $message    .= "<p>phone number: <strong>". $phone ."</strong></p></br>";
+        // $message    .= "<p>Message: <strong>". $message ."</strong></p></br>";
+        $message    .= "<p>Thank you,</p>";
+        $message    .= "<p>". $name ."</p>";
+        $message    .= "<p>" . $email . " | " . $phone . "</p>";
+
+        $headers    = "From:" . $name . " <" . $email . ">\r\n";
+        $headers    .= "Reply-To: " . $email . "\r\n";
+        $headers    .= "Content-type: text/html\r\n";
+
+        mail($to, $subject, $message, $headers);
+
+        // Redirect to thank you page
+        header('Location:thank-you.html');
     }
 
 ?>
